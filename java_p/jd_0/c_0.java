ac55ce49649d24111e9d44fde87fdb0d33a15534
Jidor Tang <tlqtangok@126.com>
2019-09-04 14:20:44 +0800

    first java demo

---
+++ java_p/jd_0/c_0.java
@@ -0,0 +1,80 @@
+package jd_0;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class c_0 {
+
+	public int x; 
+
+	public c_0(int x_)
+	{
+		x = x_;
+	}
+
+	public static String run_cmd(String shellString) {  
+
+		StringBuffer sb = new StringBuffer();
+		try {  
+			Process process = Runtime.getRuntime().exec(shellString);  
+
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			int exitValue = process.waitFor();
+
+			String line;
+
+			while((line = reader.readLine())!= null){
+				//System.out.println(line);
+				sb.append(line+"\n"); 
+			}
+
+			if (0 != exitValue) {  
+				System.out.println("- error! call shell failed. error code is :" + exitValue);  
+			}  
+		} catch (Throwable e) {  
+			System.out.println("call shell failed. " + e);  
+		}  
+		return sb.toString(); 
+	}
+
+	public void fun_0()
+	{
+		System.out.println(x); 	
+		int[] id_int = {3,2,1};
+
+		try {
+			System.out.println("- sleep 1s"); 
+			Thread.sleep(1*1000); 
+		}
+		catch (Exception e){
+			System.out.println("- error" + e.toString()); 
+		}
+		print_array(id_int); 
+
+		String fc_cmd = run_cmd(" ls -al "); 
+		//String fc_cmd = run_cmd(" find . -name '*' "); 
+		System.out.println(fc_cmd); 
+	}
+
+	public void print_array(int[] id_int)
+	{
+		for (int e_ : id_int)
+		{
+			System.out.println(e_);
+		}
+	}
+
+
+	public static void main(String [] args) {
+		//id_c_0.x = 99; 
+		c_0 id_c_0 = new c_0(888); 
+		id_c_0.fun_0(); 
+
+		System.out.println("Hello World"); // 打印 Hello World
+	}
+}
+
---
+++ java_p/jd_1/c_1.java
@@ -0,0 +1,27 @@
+package jd_1;
+
+import jd_0.*;
+
+public class c_1 {
+
+	public int x; 
+
+	public c_1(int x_)
+	{
+		x = x_;
+	}
+
+	public void fun_0()
+	{
+		System.out.println(x); 	
+	}
+
+	public static void main(String [] args) {
+		//id_c_0.x = 99; 
+		c_0 id_c_0 = new c_0(888); 
+		id_c_0.fun_0(); 
+
+		System.out.println("Hello World"); // 打印 Hello World
+	}
+}
+
